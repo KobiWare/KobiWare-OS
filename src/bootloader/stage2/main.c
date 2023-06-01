3a1ac2e8ab930d13fe31ac42b628cca6d20f2ef1
Pyro <79991107+Pyro569@users.noreply.github.com>
2023-06-01 19:38:17 -0400

    Printf Support

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -1,8 +1,16 @@
-#include "boot.c"
+#include "stdio.h"
+#include "stdint.h"
 
 void _cdecl cstart_(uint16_t bootDrive)
 {
-    bootMessages();
+    printf("Welcome to KobiWare OS!\r\n");
+    printf("\n");
+    printf("KobiWare OS is currently being actively developed by ONE person. Check out the operating system source code at https://github.com/KobiWare/KobiWare-OS!\r\n");
+    printf("\n");
+    printf("KobiWare OS is currently a work in progress, please expect bugs and please do not use this as an actual operating system.\r\n");
+    printf("\n");
+    printf("KobiWare OS is now loading...\r\n");
+
     for (;;)
         ;
 }
